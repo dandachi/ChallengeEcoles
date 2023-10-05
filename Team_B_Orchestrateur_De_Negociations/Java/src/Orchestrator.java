1bb08f6bae589e4a02b5ad9f99a0169ae4b6e240
Mohamad Dandachi <mohamad.dandachi@novencia.com>
2023-10-05 19:40:32 +0200

    Add TODO for Orchestrator

--- Team_B_Orchestrateur_De_Negociations/Java/src/Orchestrator.java
+++ Team_B_Orchestrateur_De_Negociations/Java/src/Orchestrator.java
@@ -10,19 +10,18 @@ public class Orchestrator extends OrchestratorBase {
 
     public Orchestrator(IQuoteQueues quoteQueues, IPricerQueues pricerQueues) {
         super(quoteQueues, pricerQueues);
-
-        // add code to constructor
+        // TODO Initialize data structure to hold quotes, and to hold quote prices
     }
 
     @Override
     protected void OnQuoteRequest(Quote o) {
-        // add code
+        // TODO Add quotes to the data structure
         NotifyQuoteReceived(o);
     }
 
     @Override
     protected void OnQuotePriceUpdate(QuotePrice o) {
-        // add code
+        // TODO Update quote prices in quote prices data structure
         NotifyPriceUpdated(o);
     }
 
@@ -30,6 +29,9 @@ public class Orchestrator extends OrchestratorBase {
     protected void OnExecutionRequest(QuoteExecutionPrice o) {
         // add code
         var anyValidPrice = false;
+        // TODO check if there is a quote price  from the last 3 quote prices
+        //  that is equal to execution price within EXECUTION Tolerance
+
         if (anyValidPrice)
         {
             o.Status = ExecutionStatus.Success;
@@ -40,11 +42,14 @@ public class Orchestrator extends OrchestratorBase {
             o.Status = ExecutionStatus.Fail;
             NotifyQuoteExecuted(o);
         }
+
+        // TODO clean unwanted quotes and quote prices
     }
 
     @Override
     protected void OnQuoteStop(Quote o) {
-        // add code
+
+        // TODO clean unwanted quotes and quote prices
         NotifyQuoteStopped(o);
     }
 
@@ -52,13 +57,21 @@ public class Orchestrator extends OrchestratorBase {
     public void Start() {
         super.Start();
         // add code
+        _scheduler = Executors.newScheduledThreadPool(1);
+        _scheduler.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                // TODO Schedule a pricing task
+                // We should ask for a pricing for each quote we that is alive
+                // Communication with pricer can be done using the method RequestNewPrice
+            }
+        }, 1, 2, TimeUnit.SECONDS);
 
     }
 
     @Override
     public void Stop() {
         super.Stop();
-        // add code
-
+        _scheduler.shutdown();
     }
 }
